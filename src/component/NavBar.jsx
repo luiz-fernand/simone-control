88c16941f66d62174deb720153d717043257bbe5
Luiz Fernando de Souza <f3rn.luiz@gmail.com>
2023-07-24 20:44:17 -0300

    [Edit] - fixed selected page button

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 
 import '../style/components/NavBar.css'
 
@@ -8,10 +8,10 @@ const NavBar = () => {
         <header>
             <img src={process.env.PUBLIC_URL + '/Logo.png'} alt="SCS" width={50}/>
             <nav className='nav-container'>
-                <Link className='nav-item' to='/'>Inicio</Link>
-                <Link className='nav-item' to='/'>Produtos</Link>
-                <Link className='nav-item' to='/clientes'>Clientes</Link>
-                <Link className='nav-item' to='/'>Vendas</Link>
+                <NavLink className='nav-item' activeClassName='active' to='/'>Inicio</NavLink>
+                <NavLink className='nav-item' activeClassName='active' to='/produtos'>Produtos</NavLink>
+                <NavLink className='nav-item' activeClassName='active' to='/clientes'>Clientes</NavLink>
+                <NavLink className='nav-item' activeClassName='active' to='/vendas'>Vendas</NavLink>
             </nav>
             <p style={{color: 'lightgray', fontSize: '10pt'}}>V0.1 - by: LZ TI</p>
         </header>
