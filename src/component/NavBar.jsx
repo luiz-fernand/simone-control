08a38dd71c66024d3618dbd3ad64f05b0b0668eb
Luiz Fernando de Souza <f3rn.luiz@gmail.com>
2023-07-26 15:32:32 -0300

    [Edit] - NavLink activeClassName bug solved

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -8,10 +8,10 @@ const NavBar = () => {
         <header>
             <img src={process.env.PUBLIC_URL + '/Logo.png'} alt="SCS" width={50}/>
             <nav className='nav-container'>
-                <NavLink className='nav-item' activeClassName='active' to='/'>Inicio</NavLink>
-                <NavLink className='nav-item' activeClassName='active' to='/produtos'>Produtos</NavLink>
-                <NavLink className='nav-item' activeClassName='active' to='/clientes'>Clientes</NavLink>
-                <NavLink className='nav-item' activeClassName='active' to='/vendas'>Vendas</NavLink>
+                <NavLink className='nav-item' to='/'>Inicio</NavLink>
+                <NavLink className='nav-item' to='/produtos'>Produtos</NavLink>
+                <NavLink className='nav-item' to='/clientes'>Clientes</NavLink>
+                <NavLink className='nav-item' to='/vendas'>Vendas</NavLink>
             </nav>
             <p style={{color: 'lightgray', fontSize: '10pt'}}>V0.3 - by: LZ TI</p>
         </header>
