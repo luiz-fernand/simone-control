262f244de03ab30c941c9dcff9b8b2b6bd96aeec
Luiz Fernando de Souza <f3rn.luiz@gmail.com>
2023-07-27 14:11:53 -0300

    [Edit] - home styled with the official logo

--- src/Inicio.jsx
+++ src/Inicio.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 
 import './style/Inicio.css'
 
-const Home = () => {
+const Inicio = () => {
   return (
     <div className="inicio-container">
-      <h1>Home</h1>
+      <div className="painel-info-inicio">
+
+      </div>
+      <div className="bem-vindo-inicio">
+        <img src={process.env.PUBLIC_URL + '/logo-brecho.png'} alt="logo-brecho" width={300}/>
+      </div>
+      <div className="painel-acao-inicio">
+
+      </div>
     </div>
   )
 }
  
-export default Home
+export default Inicio
