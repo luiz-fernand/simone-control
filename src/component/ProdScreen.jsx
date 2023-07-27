e96120a5d491464e46fa04ca278f965878926fd2
Luiz Fernando de Souza <f3rn.luiz@gmail.com>
2023-07-27 14:15:38 -0300

    [Add] - product screen when clicking on it

--- src/component/ProdScreen.jsx
+++ src/component/ProdScreen.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
-import {AiOutlineCloseCircle} from 'react-icons/ai'
+
+import ClientesJson from '../data/clientes.json'
+
+import {AiOutlineCloseCircle, AiOutlineEdit} from 'react-icons/ai'
+import {BsCartPlus} from 'react-icons/bs'
+import {RiDeleteBinLine} from 'react-icons/ri'
+
 import '../style/components/ProdScreen.css'
 
 const ProdScreen = ({product, onClose}) => {
+    const cliente = ClientesJson.find((cli) =>  cli.id === product.cliente)
     return (
         <div className="prod-screen-container">
-            <AiOutlineCloseCircle className='close-prod-screen' onClick={onClose} color='white'/>
-            <h1>{product.titulo}</h1>
+            <div className="prod-screen-info">
+                <AiOutlineCloseCircle className='close-prod-screen' onClick={onClose} color='white'/>
+                <p>{product.id}</p>
+                <p>Item de: {cliente.nome}</p>
+                <h1>{product.titulo}</h1>
+                <h2>Status - {product.status === 0 ? 'DISPONIVEL' : (product.status === 1 ? 'VENDIDO' : 'PAGO!')}</h2>
+                <p>{product.valor}</p>
+            </div>
+            <div className="prod-screen-acao">
+                {product.status > 0 ? null : <button><BsCartPlus style={{marginRight: '5px', fontSize: '18pt'}}/>Vender</button>}
+                <button><AiOutlineEdit style={{marginRight: '5px', fontSize: '18pt'}}/>Editar</button>
+                <button><RiDeleteBinLine style={{marginRight: '5px', fontSize: '18pt'}}/>Excluir</button>
+            </div>
         </div>
     )
 }
