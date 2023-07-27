a729ecb15820d351a145d51af55106c7ddbc559a
Luiz Fernando de Souza <f3rn.luiz@gmail.com>
2023-07-27 16:34:39 -0300

    [Edit] - styled product screen, small ajustments

--- src/component/ProdScreen.jsx
+++ src/component/ProdScreen.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 import ClientesJson from '../data/clientes.json'
 
 import {AiOutlineCloseCircle, AiOutlineEdit} from 'react-icons/ai'
-import {BsCartPlus} from 'react-icons/bs'
 import {RiDeleteBinLine} from 'react-icons/ri'
 
 import '../style/components/ProdScreen.css'
@@ -11,19 +10,20 @@ import '../style/components/ProdScreen.css'
 const ProdScreen = ({product, onClose}) => {
     const cliente = ClientesJson.find((cli) =>  cli.id === product.cliente)
     return (
-        <div className="prod-screen-container">
-            <div className="prod-screen-info">
-                <AiOutlineCloseCircle className='close-prod-screen' onClick={onClose} color='white'/>
-                <p>{product.id}</p>
-                <p>Item de: {cliente.nome}</p>
-                <h1>{product.titulo}</h1>
-                <h2>Status - {product.status === 0 ? 'DISPONIVEL' : (product.status === 1 ? 'VENDIDO' : 'PAGO!')}</h2>
-                <p>{product.valor}</p>
-            </div>
-            <div className="prod-screen-acao">
-                {product.status > 0 ? null : <button><BsCartPlus style={{marginRight: '5px', fontSize: '18pt'}}/>Vender</button>}
-                <button><AiOutlineEdit style={{marginRight: '5px', fontSize: '18pt'}}/>Editar</button>
-                <button><RiDeleteBinLine style={{marginRight: '5px', fontSize: '18pt'}}/>Excluir</button>
+        <div className="product-screen-overlay">
+            <div className="prod-screen-container">
+                <div className="prod-screen-info">
+                    <AiOutlineCloseCircle className='close-prod-screen' onClick={onClose} color='white'/>
+                    <p>{product.id}</p>
+                    <p>Item de: {cliente.nome}</p>
+                    <h1>{product.titulo}</h1>
+                    <h2>Status - {product.status === 0 ? 'DISPONIVEL' : (product.status === 1 ? 'VENDIDO' : 'PAGO!')}</h2>
+                    <p>{product.valor}</p>
+                </div>
+                <div className="prod-screen-acao">
+                    <button className='btn-prdscr-1'><AiOutlineEdit style={{marginRight: '5px', fontSize: '18pt'}}/>Editar</button>
+                    <button className='btn-prdscr-2'><RiDeleteBinLine style={{marginRight: '5px', fontSize: '18pt'}}/>Excluir</button>
+                </div>
             </div>
         </div>
     )
