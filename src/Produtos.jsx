222c9f4ad7c4837428f5a80eae0829820bb36055
Luiz Fernando de Souza <f3rn.luiz@gmail.com>
2023-07-29 01:01:31 -0300

    [Edit] - atualized products data process

--- src/Produtos.jsx
+++ src/Produtos.jsx
@@ -1,7 +1,9 @@
 import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
+import {toast, ToastContainer} from 'react-toastify'
+import axios from 'axios'
 
-import ProdutosJson from './data/produtos.json'
+// import ProdutosJson from './data/produtos.json'
 
 import {GiClothes} from 'react-icons/gi'
 import {AiOutlinePlusCircle} from 'react-icons/ai'
@@ -18,26 +20,32 @@ const Produtos = () => {
     const [selectedProduct, setSelectedProduct] = useState(null)
   
     useEffect(() => {
+        getProdutos()
+
         let totalProdutos = 0
         let disponiveis = 0
         let vendidos = 0
         let pagos = 0
-        const updateProdList = []
   
-        ProdutosJson.forEach((prod) => {
+        ProdList.forEach((prod) => {
             totalProdutos++
             (prod.status === 0 ? disponiveis++ : (prod.status === 1 ? vendidos++ : pagos++))
-
-            const itemLista = {cliente: prod.cliente, id: `#${prod.cliente}-${prod.id}`, tipo: prod.tipo, titulo: `${prod.tipo} ${prod.descricao}`, valor: `R$ ${prod.valor}`, status: prod.status}
-            updateProdList.push(itemLista)
         })
   
         setNp(totalProdutos)
         setPd(disponiveis)
         setPv(vendidos)
         setPp(pagos)
-        setProdList(updateProdList)
-    }, [])
+    }, [ProdList])
+
+    const getProdutos = async () => {
+        try {
+            const res = await axios.get('http://localhost:8800/produtos')
+            setProdList(res.data)
+        } catch(error){
+            toast.error(error)
+        }
+    }
 
     const openProdScreen = (prod) => {
         setSelectedProduct(prod)
@@ -69,12 +77,13 @@ const Produtos = () => {
             <div className="lista-produtos">
                 {ProdList.map(prod => (
                     <div className={'cod-produto' + (prod.status === 2 ? ' indisp' : (prod.status === 1 ? ' process' : ''))} onClick={() => openProdScreen(prod)} key={prod.id}>
-                        <p style={{flex: '1', textAlign: 'start', marginLeft: '25px'}}>{prod.id}</p>
-                        <p style={{flex: '2'}}><b>{prod.titulo}</b></p>
-                        <p style={{flex: '1'}}>{prod.valor}</p>
+                        <p style={{flex: '1', textAlign: 'start', marginLeft: '25px'}}>{`#${prod.cliente}-${prod.id}`}</p>
+                        <p style={{flex: '2'}}><b>{`${prod.tipo} ${prod.descricao}`}</b></p>
+                        <p style={{flex: '1'}}>{`R$ ${prod.valor}`}</p>
                     </div>
                 ))}
             </div>
+            <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_CENTER}/>
             {selectedProduct && <ProdScreen product={selectedProduct} onClose={closeProdScreen} excluirProd={excluirProduto}/>}
         </div>
     )
