40e7378ea9be7055d96dec4f5a387620ad997286
Luiz Fernando de Souza <f3rn.luiz@gmail.com>
2023-07-30 11:40:47 -0300

    [Edit] - update stats functions

--- src/Clientes.jsx
+++ src/Clientes.jsx
@@ -6,11 +6,21 @@ import './style/Clientes.css'
 
 const Clientes = () => {
     const [ClienList, setClienList] = useState([])
+    const [ProdList, setProdList] = useState([])
+    const [ClienPen, setClienPen] = useState(0)
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     useEffect(() => {
         getClientes()
-        console.log('deu certo!')
-    }, [])
+        getProdutos()
+        let pen = 0
+
+        ProdList.forEach((prod) => {
+            if(prod.status === 1) pen++
+        })
+
+        setClienPen(pen)
+    })
 
     const getClientes = async () => {
         try {
@@ -21,19 +31,39 @@ const Clientes = () => {
         }
     }
 
+    const getProdutos = async () => {
+        try {
+            const res = await axios.get('http://localhost:8800/produtos')
+            setProdList(res.data)
+        } catch(error){
+            window.alert(error)
+        }
+    }
+
+    const ProdCli = (id) => {
+        let cont = [0, 0]
+        ProdList.forEach((pro) => {
+            if(pro.cliente === id){
+                cont[0]++
+                if(pro.status === 1) cont[1]++
+            }
+        })
+        return cont
+    }
+
     return (
         <div className="clientes-container">
             <div className="informacoes-clientes">
                 <h1 className='titulo-clientes'><BsPeopleFill style={{marginRight: '10px'}}/> Clientes</h1>
                 <p>Nº DE CLIENTES: <b>{ClienList.length}</b></p>
-                <p>CLIENTES PENDENTES: <b>{ClienList.length}</b></p>
+                <p>CLIENTES PENDENTES: <b>{ClienPen}</b></p>
             </div>
             <div className="lista-clientes">
                 {ClienList.map((clien) => (
-                    <div className={'cod-cliente' + (0 > 1 ? ' cliente-pendente' : '')} key={clien.id}>
+                    <div className={'cod-cliente' + (ProdCli(clien.id)[1] > 0 ? ' cliente-pendente' : '')} key={clien.id}>
                         <p style={{flex: '1', textAlign: 'start', marginLeft: '25px'}}>#{clien.id}</p>
                         <p style={{flex: '2'}}><b>{clien.nome}</b></p>
-                        <p style={{flex: '1'}}>Produtos: {0}</p>
+                        <p style={{flex: '1'}}>Produtos: {ProdCli(clien.id)[0]}</p>
                     </div>
                 ))}
             </div>
