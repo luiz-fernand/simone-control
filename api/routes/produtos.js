6f3764e99f9b8a9878b7943f9c8d01e51d13a736
Luiz Fernando de Souza <f3rn.luiz@gmail.com>
2023-07-30 10:12:06 -0300

    [Edit] - put > get

--- api/routes/produtos.js
+++ api/routes/produtos.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router.get('/', getProdutos)
 router.post('/', addProduto)
-router.put('/:id', getProdutoById)
+router.get('/:id', getProdutoById)
 router.put('/:id', editProduto)
 router.delete('/:id', delProduto)
 
