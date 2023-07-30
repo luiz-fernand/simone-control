9a4fc1495a7ac36fb98e6c507b393501c7f6ea60
Luiz Fernando de Souza <f3rn.luiz@gmail.com>
2023-07-30 10:03:53 -0300

    [Edit] - Clientes page passed to new data schema

--- src/Clientes.jsx
+++ src/Clientes.jsx
@@ -1,36 +1,39 @@
-import React from 'react'
-
-import ClientesJson from './data/clientes.json'
-import ProdutosJson from './data/produtos.json'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 import {BsPeopleFill} from 'react-icons/bs'
 import './style/Clientes.css'
 
 const Clientes = () => {
-    
-    const QtdProdCliente = (cliente) => {
-        let n = [0, 0]
-        ProdutosJson.forEach((prod) => {
-            if(cliente.id === prod.cliente){
-                n[0]++
-                if(prod.status === 1) n[1]++
-            }
-        })
-        return n
+    const [ClienList, setClienList] = useState([])
+
+    useEffect(() => {
+        getClientes()
+        console.log('deu certo!')
+    }, [])
+
+    const getClientes = async () => {
+        try {
+            const res = await axios.get('http://localhost:8800/clientes')
+            setClienList(res.data)
+        } catch(error){
+            window.alert(error)
+        }
     }
 
     return (
         <div className="clientes-container">
             <div className="informacoes-clientes">
                 <h1 className='titulo-clientes'><BsPeopleFill style={{marginRight: '10px'}}/> Clientes</h1>
-                <p>Nº DE CLIENTES: <b>{ClientesJson.length}</b></p>
+                <p>Nº DE CLIENTES: <b>{ClienList.length}</b></p>
+                <p>CLIENTES PENDENTES: <b>{ClienList.length}</b></p>
             </div>
             <div className="lista-clientes">
-                {ClientesJson.map(clien => (
-                    <div className={'cod-cliente' + (QtdProdCliente(clien)[1] > 0 ? ' cliente-pendente' : '')} key={clien.id}>
+                {ClienList.map((clien) => (
+                    <div className={'cod-cliente' + (0 > 1 ? ' cliente-pendente' : '')} key={clien.id}>
                         <p style={{flex: '1', textAlign: 'start', marginLeft: '25px'}}>#{clien.id}</p>
                         <p style={{flex: '2'}}><b>{clien.nome}</b></p>
-                        <p style={{flex: '1'}}>Produtos: {QtdProdCliente(clien)[0]}</p>
+                        <p style={{flex: '1'}}>Produtos: {0}</p>
                     </div>
                 ))}
             </div>
