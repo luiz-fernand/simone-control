1922c09999bc36a7ef0a9f96d8beeb35d57cbc99
Luiz Fernando de Souza <f3rn.luiz@gmail.com>
2023-08-07 22:52:21 -0300

    [Edit] - pre styled the page

--- src/Vendas.jsx
+++ src/Vendas.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { BsShop } from 'react-icons/bs'
+import { MdAttachMoney } from 'react-icons/md'
+import './style/Vendas.css'
 
 const Vendas = () => {
     return (
-        <div className="vendas-container">
-            <h1>Vendas</h1>
+        <div className="vendas-container fbrc">
+            <div className="informacoes-vendas fbcc">
+                <h1 className="titulo-vendas fbrc"><BsShop style={{marginRight: '10px'}}/> Vendas</h1>
+                <div className="vendas-acao-container fbcc">
+                    <Link className='fbrc'><MdAttachMoney style={{marginRight: '5px', fontSize: '20pt'}}/>VENDER</Link>
+                </div>
+            </div>
+            <div className="lista-vendas fbcc">
+
+            </div>
         </div>
     )
 }
  
-export default Vendas;
+export default Vendas
