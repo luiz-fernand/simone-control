01cc41163008184e46ad19b011c618cd37db10c5
Luiz Fernando de Souza <f3rn.luiz@gmail.com>
2023-08-22 16:53:29 -0300

    [Add] - comissoes data page

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import prodRoutes from './routes/produtos.js'
 import clieRoutes from './routes/clientes.js'
 import vendRoutes from './routes/vendas.js'
+import comissRoutes from './routes/comissoes.js'
 
 const app = express()
 
@@ -12,5 +13,6 @@ app.use(cors())
 app.use("/clientes", clieRoutes)
 app.use("/produtos", prodRoutes)
 app.use("/vendas", vendRoutes)
+app.use("/comissoes", comissRoutes)
 
 app.listen(8800)
