fa28e4a230fb6bc3f7f9550e29413872393d3fa5
Luiz Fernando de Souza <f3rn.luiz@gmail.com>
2023-08-22 16:54:09 -0300

    [Edit] - comissoes list function

--- src/Comissoes.jsx
+++ src/Comissoes.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { BiMoney } from 'react-icons/bi'
 import { GiPayMoney } from 'react-icons/gi'
@@ -6,6 +7,35 @@ import { GiPayMoney } from 'react-icons/gi'
 import './style/Comissoes.css'
 
 const Comissoes = () => {
+    const [ Comissoes, setComissoes ] = useState([])
+    const [ ValorTT, setValorTT ] = useState(0.0)
+
+    useEffect(() => {
+        getComissoes()
+    }, [])
+
+    const getComissoes = async () => {
+        try {
+            const res = await axios.get('http://localhost:8800/comissoes')
+            const preLista = []
+            let vtt = 0.0
+
+            res.data.forEach((com) => {
+                vtt += com.valor
+                const jsonTo = JSON.parse(com.produtos)
+                const newData = new Date(com.data)
+                const attNewData = `${ newData.getDate().toString().padStart(2,'0') }/${ String(newData.getMonth() + 1).padStart(2,'0') }/${ newData.getFullYear() } - ${ com.hora }`
+                const itemLista = { ...com, produtos: jsonTo, data: attNewData }
+                preLista.push(itemLista)
+            })
+
+            setValorTT(vtt)
+            setComissoes(preLista)
+        } catch(error){
+            console.log(error)
+        }
+    }
+
     return (
         <div className="comissoes-container fbrc">
             <div className="menu-comissoes fbcc">
@@ -24,12 +54,22 @@ const Comissoes = () => {
                         <input type="date" name="data2" id="data-venda-2" />
                     </div>
                 </div>
-                <p style={{marginTop: '15px'}}>Nº DE COMISSÕES: <b>-</b></p>
-                <p>COMISSÕES PENDENTES: <b>-</b></p>
-                <p>VALOR DAS COMISSÕES: <b>R$ -</b></p>
+                <p style={{marginTop: '15px'}}>Nº DE COMISSÕES: <b>{ Comissoes.length }</b></p>
+                <p>VALOR DAS COMISSÕES: <b>R$ { ValorTT }</b></p>
             </div>
             <div className="lista-comissoes fbcc">
-                
+                {Comissoes.map((com) => (
+                    <div className="item-venda fbcc" key={com.id}>
+                        <div className="id-box-item-venda fbrc">
+                            <p>#{com.id}</p>
+                            <p>{com.data}</p>
+                        </div>
+                        <div className="fbcc" style={{ width: '85%' }}>
+                            <p className='desc-item-vend'><b>{com.descricao}</b></p>
+                            <p style={{ borderTop: '1px solid #ccc', margin: 0, padding: '15px 0', width: '100%' }}>TOTAL: <b>R$ {com.valor}</b></p>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     )
