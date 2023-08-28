bdb4d4e0965a037794c654567e2fa75cd2432bc5
Luiz Fernando de Souza <f3rn.luiz@gmail.com>
2023-08-28 10:00:45 -0300

    [Edit] - edit comissao function finalized

--- src/component/function/editCom.jsx
+++ src/component/function/editCom.jsx
@@ -10,6 +10,7 @@ const EditarComissao = () => {
     const { id } = useParams()
     const [ Comissao, setComissao ] = useState([])
     const [ Produtos, setProdutos ] = useState([])
+    const [ Cliente, setCliente ] = useState([])
 
     useEffect(() => {
         document.documentElement.style.pointerEvents = 'all'
@@ -18,7 +19,6 @@ const EditarComissao = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
     
-    console.log(Comissao)
     const getProdutos = async () => {
         try {
             const res = await axios.get('http://localhost:8800/produtos/')
@@ -31,11 +31,13 @@ const EditarComissao = () => {
     const getComissao = async () => {
         try {
             const res = await axios.get(`http://localhost:8800/comissoes/${id}`)
+            const resC = await axios.get(`http://localhost:8800/clientes/${res.data[0]?.cliente}`)
 
             const newData = String(res.data[0]?.data).split('T')[0]
             const jsonProdutos = JSON.parse(res.data[0]?.produtos)
             const preCom = {...res.data[0], produtos: jsonProdutos, data: newData}
 
+            setCliente(resC.data[0])
             setComissao(preCom)
         } catch (error) {
             console.log(error)
@@ -44,20 +46,21 @@ const EditarComissao = () => {
 
     const editarComissao = async (e) => {
         e.preventDefault()
-        // const parsed = JSON.stringify(Venda?.produtos)
+        const parsed = JSON.stringify(Comissao?.produtos)
 
-        // await axios.put(`http://localhost:8800/vendas/${id}`,{
-        //     descricao: Venda?.descricao,
-        //     produtos: parsed,
-        //     data: Venda?.data,
-        //     hora: Venda?.hora,
-        //     valortotal: Venda?.valortotal
-        // })
-        // .then(({data}) => {
-        //     window.alert(data)
-        //     window.location.replace('http://localhost:3000/vendas')
-        // })
-        // .catch((error) => console.log(error))
+        await axios.put(`http://localhost:8800/comissoes/${id}`,{
+            cliente: Comissao?.cliente,
+            descricao: Comissao?.descricao,
+            produtos: parsed,
+            data: Comissao?.data,
+            hora: Comissao?.hora,
+            valor: Comissao?.valor
+        })
+        .then(({data}) => {
+            window.alert(data)
+            window.location.replace('http://localhost:3000/comissoes')
+        })
+        .catch((error) => console.log(error))
     }
 
     const handleDescriptionChange = (e) => {
@@ -129,19 +132,22 @@ const EditarComissao = () => {
             </div>
             <div className="box-itens-venda fbcc">
                 <div className="itens-lista-add-vend fbcc">
-                    {/* {Venda.produtos !== undefined ? Venda.produtos.map((p) => {
-                        const idxProd = ProdList.findIndex((pro) => pro.id === p.id)
+                    <div className={`item-disp-add-venda fbrc pro-select-add-vend`} style={{ marginTop: '15px' }}>
+                        <p style={{ flex: 0.5, marginLeft: '15px' }}>{`#${ Cliente?.id }`}</p>
+                        <p style={{ flex: 1 }}>{`${ Cliente?.nome }`}</p>
+                        <p style={{ flex: 0.5 }}>{`Produtos: ${ Comissao.produtos?.length }`}</p> 
+                    </div>
+                    {Comissao.produtos?.map((p) => {
+                        const idxProd = Produtos.findIndex((pro) => pro.id === p.id)
                         return (
-                            (ProdList[idxProd].tipo.toLowerCase() + ' ' + ProdList[idxProd].descricao.toLowerCase()).includes(procurarProduto.toLowerCase()) ? (
-                                <div className={`item-disp-add-venda fbrc pro-select-add-vend`} key={p.id}>
-                                    <p>{`#${ ProdList[idxProd].cliente }-${ ProdList[idxProd].id }`}</p>
-                                    <p>{`${ ProdList[idxProd].tipo } ${ ProdList[idxProd].descricao }`}</p>
-                                    <p>{ ProdList[idxProd].tamanho }</p>
-                                    <p>{`R$ ${ ProdList[idxProd].valor }`}</p>
-                                </div>
-                            ) : null
+                            <div className={`item-disp-add-venda fbrc pro-select-add-vend`} style={{ marginLeft: '15px', fontSize: '11pt', padding: '2px 15px' }} key={p.id}>
+                                <p style={{ flex: 0.7 }}>{`#${ Produtos[idxProd].cliente }-${ Produtos[idxProd].id }`}</p>
+                                <p style={{ flex: 2 }}>{`${ Produtos[idxProd].tipo } ${ Produtos[idxProd].descricao }`}</p>
+                                <p style={{ flex: 0.3 }}>{ Produtos[idxProd].tamanho }</p>
+                                <p style={{ flex: 0.5 }}>{`R$ ${ Produtos[idxProd].valor/2 }`}</p>
+                            </div>
                         )
-                    }) : null} */}
+                    })}
                 </div>
             </div>
         </div>
