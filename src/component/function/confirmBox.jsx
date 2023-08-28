51d90a92283e518eedeca4c35bd0bed9d99ba90e
Luiz Fernando de Souza <f3rn.luiz@gmail.com>
2023-08-28 10:35:10 -0300

    [Edit] - style att, buttons animation and box size

--- src/component/function/confirmBox.jsx
+++ src/component/function/confirmBox.jsx
@@ -18,9 +18,9 @@ const ConfirmBox = ({ item, desfazer, excluir, onClose }) => {
             <div className="cbContainer fbcc">
                 <p>Escolha uma das opções abaixo:</p>
                 <div className="button-list-confirm fbrc">
-                    <button style={{ backgroundColor: '#3498db' }} onClick={() => confirmDesfazer()}>Desfazer</button>
-                    <button style={{ backgroundColor: '#e74c3c' }} onClick={() => confirmExcluir()}>Excluir</button>
-                    <button style={{ backgroundColor: '#ccc', color: 'black' }} onClick={onClose}>Cancelar</button>
+                    <button id='btn-1' onClick={() => confirmDesfazer()}>Desfazer</button>
+                    <button id='btn-2' onClick={() => confirmExcluir()}>Excluir</button>
+                    <button id='btn-3' onClick={onClose}>Cancelar</button>
                 </div>
             </div>
         </div>
