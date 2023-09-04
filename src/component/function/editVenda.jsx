58d29120cee530da82d36b008bdfc082fc439159
Luiz Fernando de Souza <f3rn.luiz@gmail.com>
2023-09-04 11:45:41 -0300

    [Add] - add and remove product function

--- src/component/function/editVenda.jsx
+++ src/component/function/editVenda.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom'
 import axios from 'axios'
 
 import { GiConfirmed } from 'react-icons/gi'
+import { AiOutlineSend } from 'react-icons/ai'
 import '../../style/components/function/addVendas.css'
 
 const EditarVenda = () => {
@@ -10,7 +11,7 @@ const EditarVenda = () => {
     const { id } = useParams()
     const [ Venda, setVenda ] = useState([])
     const [ ProdList, setProdList ] = useState([])
-    const [ procurarProduto, setProcurarProduto ] = useState('')
+    const [ ProdRem, setProdRem ] = useState([])
 
     useEffect(() => {
         document.documentElement.style.pointerEvents = 'all'
@@ -42,24 +43,6 @@ const EditarVenda = () => {
         }
     }
 
-    const editarVenda = async (e) => {
-        e.preventDefault()
-        const parsed = JSON.stringify(Venda?.produtos)
-
-        await axios.put(`http://localhost:8800/vendas/${id}`,{
-            descricao: Venda?.descricao,
-            produtos: parsed,
-            data: Venda?.data,
-            hora: Venda?.hora,
-            valortotal: Venda?.valortotal
-        })
-        .then(({data}) => {
-            window.alert(data)
-            window.location.replace('http://localhost:3000/vendas')
-        })
-        .catch((error) => console.log(error))
-    }
-
     const handleDescriptionChange = (e) => {
         const newDescription = e.target.value;
         setVenda((prevVenda) => ({ ...prevVenda, descricao: newDescription }));
@@ -73,6 +56,99 @@ const EditarVenda = () => {
     const handleTimeChange = (e) => {
         const newTime = e.target.value;
         setVenda((prevVenda) => ({ ...prevVenda, hora: newTime }));
+    }
+
+    const editarProd = async (id, stt) => {
+        const prodID = ProdList.findIndex((prod) => prod.id === id)
+
+        await axios.put(`http://localhost:8800/produtos/${id}`,{
+            tipo: ProdList[prodID].tipo,
+            descricao: ProdList[prodID].descricao,
+            tamanho: ProdList[prodID].tamanho,
+            valor: ProdList[prodID].valor,
+            status: stt
+        })
+        .then(({data}) => {
+            console.log(data)
+        })
+        .catch(({data}) => window.alert(data))
+    }
+
+    const removerProduto = (id) => {
+        const prods = Venda.produtos
+        const idx = prods.findIndex((p) => p.id === id)
+        const idxR = ProdRem.findIndex((p) => p.id === id)
+        let valor = 0
+
+        prods.splice(idx, 1)
+
+        prods.forEach((p) => {
+            const idxP = ProdList.findIndex((pro) => pro.id === p.id)
+            valor += ProdList[idxP].valor
+        })
+
+        if(window.confirm(`Deseja realmente excluir o produto ${id} da venda?`)){
+            if(idxR < 0){
+                const pds = ProdRem
+                pds.push({ id: id })
+                setProdRem(pds)
+            }
+            setVenda((vend) => ({ ...vend, produtos: prods, valortotal: valor }))
+        }
+    }
+
+    const adicionarProduto = (id) => {
+        const prods = Venda.produtos
+        const idx = ProdList.findIndex((p) => p.id === id)
+        const idxR = ProdRem.findIndex((p) => p.id === id)
+        const ver = prods.find((p) => p.id === id)
+        let valor = 0
+        
+        if(!ver){
+            if(idx < 0) window.alert('O produto NÃO existe!')
+            else {
+                if(ProdList[idx].status !== 0) window.alert('O produto NÃO está disponivel!')
+                else {
+                    prods.push({ id: id })
+                    prods.forEach((p) => {
+                        const idxP = ProdList.findIndex((pro) => pro.id === p.id)
+                        valor += ProdList[idxP].valor
+                    })
+                    if(window.confirm(`Deseja adicionar o produto ${id} na venda?`)){
+                        if(idxR !== -1){
+                            const pds = ProdRem
+                            pds.splice(idxR, 1)
+                            setProdRem(pds)
+                        }
+                        setVenda((vend) => ({ ...vend, produtos: prods, valortotal: valor }))
+                    }
+                }
+            }
+        }
+        else window.alert('O produto já está incluso!')
+        console.log(prods)
+    }
+
+    const editarVenda = async (e) => {
+        e.preventDefault()
+
+        Venda.produtos.forEach((p) => editarProd(p.id, 1))
+        const parsed = JSON.stringify(Venda?.produtos)
+
+        ProdRem?.forEach((p) => editarProd(p.id, 0))
+
+        await axios.put(`http://localhost:8800/vendas/${id}`,{
+            descricao: Venda?.descricao,
+            produtos: parsed,
+            data: Venda?.data,
+            hora: Venda?.hora,
+            valortotal: Venda?.valortotal
+        })
+        .then(({data}) => {
+            window.alert(data)
+            window.location.replace('http://localhost:3000/vendas')
+        })
+        .catch((error) => console.log(error))
     }
 
     return (
@@ -128,16 +204,19 @@ const EditarVenda = () => {
                 </form>
             </div>
             <div className="box-itens-venda fbcc">
-                <input type="text" id='pesquisa-add-vend' placeholder='Adicionar item por ID...' onChange={(e) => setProcurarProduto(e.target.value)}/>
+                <div className="fbrc">
+                    <input type="number" id='pesquisa-add-vend' placeholder='Adicionar item por ID...'/>
+                    <AiOutlineSend  style={{ margin: '0 5px', fontSize: '25pt', cursor: 'pointer' }} onClick={() => adicionarProduto(parseInt(document.getElementById('pesquisa-add-vend').value))}/>
+                </div>
                 <div className="itens-lista-add-vend fbcc">
                     {Venda.produtos !== undefined ? Venda.produtos.map((p) => {
                         const idxProd = ProdList.findIndex((pro) => pro.id === p.id)
                         return (
-                            <div className={`item-disp-add-venda fbrc pro-select-add-vend`} key={p.id}>
-                                <p style={{ flex: 0.61, paddingLeft: '10px' }}>{`#${ ProdList[idxProd].cliente }-${ ProdList[idxProd].id }`}</p>
-                                <p style={{ flex: 2 }}>{`${ ProdList[idxProd].tipo } ${ ProdList[idxProd].descricao }`}</p>
-                                <p style={{ flex: 0.3 }}>{ ProdList[idxProd].tamanho }</p>
-                                <p style={{ flex: 0.4 }}>{`R$ ${ ProdList[idxProd].valor }`}</p>
+                            <div className={`item-disp-add-venda fbrc pro-select-add-vend`} key={p.id} onClick={() => removerProduto(p.id)}>
+                                <p style={{ flex: 0.61, paddingLeft: '10px' }}>{`#${ ProdList[idxProd]?.cliente }-${ ProdList[idxProd]?.id }`}</p>
+                                <p style={{ flex: 2 }}>{`${ ProdList[idxProd]?.tipo } ${ ProdList[idxProd]?.descricao }`}</p>
+                                <p style={{ flex: 0.3 }}>{ ProdList[idxProd]?.tamanho}</p>
+                                <p style={{ flex: 0.4 }}>{`R$ ${ ProdList[idxProd]?.valor }`}</p>
                             </div>
                         )
                     }) : null}
